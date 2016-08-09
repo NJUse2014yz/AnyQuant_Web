b5f39d3e7f6e209b97034000ccedfe4062e13de9
yfraquelle <y.f.raquelle@gmail.com>
2016-08-09 19:25:47 +0800

    user modify

--- AnyQuant_Web/src/main/java/mapper/UserInfMapper.java
+++ AnyQuant_Web/src/main/java/mapper/UserInfMapper.java
@@ -1,5 +1,6 @@
 package mapper;
 
+import po.ModifyPack;
 import po.UserInf;
 
 public interface UserInfMapper {
@@ -7,4 +8,8 @@ public interface UserInfMapper {
 	public void signUp(UserInf userInf) throws Exception;
 	/**用户查询*/
 	public UserInf select(String userName) throws Exception;
+	/**修改用户名*/
+	public void modifyUserName(ModifyPack modifyPack) throws Exception;
+	/**修改密码*/
+	public void modifyPassword(ModifyPack modifyPack) throws Exception;
 }
---
+++ AnyQuant_Web/src/main/java/po/ModifyPack.java
@@ -0,0 +1,69 @@
+package po;
+
+public class ModifyPack {
+	public String str_key;
+	public int i_key;
+	public double d_key;
+	public String str_old;
+	public String str_new;
+	public int i_old;
+	public int i_new;
+	public double d_old;
+	public double d_new;
+	
+	public String getStr_key() {
+		return str_key;
+	}
+	public void setStr_key(String str_key) {
+		this.str_key = str_key;
+	}
+	public int getI_key() {
+		return i_key;
+	}
+	public void setI_key(int i_key) {
+		this.i_key = i_key;
+	}
+	public double getD_key() {
+		return d_key;
+	}
+	public void setD_key(double d_key) {
+		this.d_key = d_key;
+	}
+	public String getStr_old() {
+		return str_old;
+	}
+	public void setStr_old(String str_old) {
+		this.str_old = str_old;
+	}
+	public String getStr_new() {
+		return str_new;
+	}
+	public void setStr_new(String str_new) {
+		this.str_new = str_new;
+	}
+	public int getI_old() {
+		return i_old;
+	}
+	public void setI_old(int i_old) {
+		this.i_old = i_old;
+	}
+	public int getI_new() {
+		return i_new;
+	}
+	public void setI_new(int i_new) {
+		this.i_new = i_new;
+	}
+	public double getD_old() {
+		return d_old;
+	}
+	public void setD_old(double d_old) {
+		this.d_old = d_old;
+	}
+	public double getD_new() {
+		return d_new;
+	}
+	public void setD_new(double d_new) {
+		this.d_new = d_new;
+	}
+	
+}
